00c04d9c2d031c0da04702092d65b325b25ab2dc
Seiichi FURUTA <seiichi.furuta.2020@gmail.com>
2022-06-07 06:52:10 +0000

    Initial Commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("START")
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html>\n<body>\n")
+	fmt.Fprintf(w, "<h1>Success!!</h1>\n")
+	fmt.Fprintf(w, "</body>\n<html>\n")
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+}
